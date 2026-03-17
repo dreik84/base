5de225718694708e0427e8959fdfcccb2f11e6a6
dreik84 <dreik84@mail.ru>
2026-03-17 16:09:41 +0300

    Update BitMask

--- src/main/java/org/example/BitMask.java
+++ src/main/java/org/example/BitMask.java
@@ -11,7 +11,55 @@ public class BitMask {
 
         TextEnum textEnum = new TextEnum();
         textEnum.applyStyle(EnumSet.of(TextEnum.Style.BOLD, TextEnum.Style.ITALIC));
+
+        System.out.println(isSetBit(7, 2));
+        System.out.println(isSetBit(7, 3));
+        System.out.println(setBit(5, 1));
+        System.out.println(inverseBit(7, 1));
+        System.out.println(resetBit(7, 1));
     }
+
+    // 0000 0111 num = 7
+    // 0000 0010 index = 2
+    // 0000 0100 1 << index
+    // 0000 0100 != 0 true
+
+    // проверка бита
+    private static boolean isSetBit(int num, int index) {
+        return (num & (1 << index)) != 0;
+    }
+
+    // 0000 0101 num = 5
+    // 0000 0001 index = 1
+    // 0000 0010 1 << index
+    // 0000 0111 | return 7
+
+    // установка бита в 1
+    private static int setBit(int num, int index) {
+        return (num | (1 << index));
+    }
+
+    // 0000 0111 num = 7
+    // 0000 0001 index = 1
+    // 0000 0010 1 << index
+    // 0000 0101 ^ return 5
+
+    // инверсия бита
+    private static int inverseBit(int num, int index) {
+        return (num ^ (1 << index));
+    }
+
+    // 0000 0111 num = 7
+    // 0000 0001 index = 1
+    // 0000 0010 1 << index
+    // 1111 1101 ~
+    // 0000 0101 & return 5
+
+    // установка бита в 0
+    private static int resetBit(int num, int index) {
+        return (num & ~(1 << index));
+    }
+
 
     public static class Text {
 
@@ -20,13 +68,15 @@ public class BitMask {
         public static final int STYLE_UNDERLINE = 1 << 2; // 4
         public static final int STYLE_STRIKE = 1 << 4; // 8
 
-        public void applyStyle(int styles) {}
+        public void applyStyle(int styles) {
+        }
     }
 
     public static class TextEnum {
 
-        public enum Style { BOLD, ITALIC, UNDERLINE, STRIKE }
+        public enum Style {BOLD, ITALIC, UNDERLINE, STRIKE}
 
-        public void applyStyle(Set<Style> styles) {}
+        public void applyStyle(Set<Style> styles) {
+        }
     }
 }
